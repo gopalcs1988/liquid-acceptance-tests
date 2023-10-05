e891e281da73e7dd5d1d15d5b81e3571bf5a2adf
Rajagopal Arumugam <Rajagopal.Arumugam@genesys.com>
2023-10-05 21:03:43 +0530

    Updated the hook and gulpfile to support the snapshot on failures

--- features/utils/hook.js
+++ features/utils/hook.js
@@ -1,17 +1,27 @@
-const { After, Before, AfterAll } = require("@cucumber/cucumber");
+var { After, Before, AfterAll, Status } = require("@cucumber/cucumber");
 const { initDriver } = require("../utils/driverUtil");
 var reporter = require('cucumber-html-reporter');
 let driver;
+
 
 Before(async function () {
   driver = await initDriver();
 });
 
-After(async function () {
+After(async function (scenario) {
+  var world = this
   try {
     if (driver) {
+
       console.log("After")
-      await driver.quit();
+      if (scenario.result.status === Status.FAILED) {
+        console.log("Snapshot")
+        return driver.takeScreenshot().then((png) => {
+          // screenShot is a base-64 encoded PNG
+          world.attach(png, 'image/png');
+      });
+      }
+    await driver.quit();
     }
   } catch (error) {
     // Handle any errors that occur while quitting the driver
--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,10 @@ var options = {
     jsonFile: './features/reports/test.json',
     output: './features/reports/cucumber_report.html',
     reportSuiteAsScenarios: true,
-    launchReport: true
+    launchReport: true,
+    screenshotsDirectory: './features/screenshots/',
+    storeScreenshots: true,
+    failedSummaryReport: true
 };
 gulp.task('CucumberReports', function () {
     reporter.generate(options);
